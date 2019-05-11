362d8998c1df83da6bcddc2e90e7c103bb710e1b
Seshien <maciek22999@wp.pl>
2019-05-11 13:55:20 +0200

    Some work donev2

--- Algoreturno/AdjacencyMatriceGraph.h
+++ Algoreturno/AdjacencyMatriceGraph.h
@@ -5,8 +5,6 @@ public:
 	AdjacencyMatriceGraph(const std::vector<std::pair<int, int>> & eL, size_t V);
 	~AdjacencyMatriceGraph();
 
-	// Inherited via GrafRepresentation
-	//virtual void show() const override;
 	void show();
 	bool addConnection(const std::pair<int, int> & c);
 
@@ -14,9 +12,6 @@ public:
 
 private:
 	std::vector<std::vector<int>> matrix;
-
-	//virtual std::vector<int> getVertices()const override;
-	//virtual std::vector<int> getNeighbours(int v)const override;
 
 	std::vector<int> getVertices();
 	std::vector<int> getNeighbours(int v);
