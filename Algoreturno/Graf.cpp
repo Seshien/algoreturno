58b4fc6bfd2d5982cff55114946e31cc4e7d4d55
Piotr Futymski <piotr.futymski@wp.pl>
2019-05-13 12:02:50 +0200

    fleury for directed and undirected

--- Algoreturno/Graf.cpp
+++ Algoreturno/Graf.cpp
@@ -33,24 +33,25 @@ void UndirectedGraf::init(const std::vector<std::pair<int, int>>& eL, size_t V)
 
 std::vector<int> UndirectedGraf::flores()
 {
+	auto G_prim = *this;
 	std::vector<int> D;
-	D.resize(this->getSize());
+	D.resize(G_prim.getSize());
 	std::vector<int> S;
-	auto v = this->getEven();
+	auto v = G_prim.getEven();
 	while (true)
 	{
 		S.push_back(v);
-		auto n = this->getNeighbours(v);
+		auto n = G_prim.getNeighbours(v);
 		if (n.size() == 0)
 			return S;
 		auto u = n[0];
 		std::fill(D.begin(), D.end(), 0);
 		auto cv = 1;
-		this->DFSb(v, -1, D, cv);
+		G_prim.DFSb(v, -1, D, cv);
 		int i = 0;
 		while (i < n.size() && matrix[v][u] == 2)
 			u = n[i++];
-		this->removeConnection({ v, u });
+		G_prim.removeConnection({ v, u });
 		v = u;
 	}
 	
@@ -163,3 +164,128 @@ int UndirectedGraf::DFSb(int v, int vf, std::vector<int>& D, int & cv)
 		matrix[vf][v] = 2;
 	return L;
 }
+
+DirectedGraf::DirectedGraf()
+{
+}
+
+DirectedGraf::DirectedGraf(const std::vector<std::pair<int, int>>& eL, size_t V)
+{
+	this->init(eL, V);
+}
+
+DirectedGraf::~DirectedGraf()
+{
+}
+
+void DirectedGraf::init(const std::vector<std::pair<int, int>>& eL, size_t V)
+{
+	elListo.resize(V);
+
+	for (auto connection : eL)
+	{
+		elListo[connection.first].push_back(connection.second);
+	}
+}
+
+std::vector<int> DirectedGraf::flores()
+{
+	auto res = std::vector<int>();
+	auto S = std::stack<int>();
+	auto G_prim = *this;
+	
+	auto v = G_prim.getEven();
+	res.push_back(v);
+	while(true)
+	{
+		while (G_prim.getNeighbours(v).size() != 0)
+		{
+			auto w = G_prim.getNeighbours(v)[0];
+			S.push(v);
+			G_prim.removeConnection({ v,w });
+			v = w;
+		}
+		if (S.size() != 0)
+		{
+			v = S.top();
+			S.pop();
+			res.push_back(v);
+		}
+		else
+		{
+			std::reverse(res.begin(), res.end());
+			return res;
+		}
+	}
+}
+
+void DirectedGraf::show()
+{
+	std::cout << "Graf jak list sasiedztwa:" << std::endl;
+	for (size_t i = 0; i < elListo.size(); i++)
+	{
+		std::cout << i << ":\t";
+		for (size_t j = 0; j < elListo[i].size(); j++)
+		{
+			std::cout << elListo[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+bool DirectedGraf::addConnection(const std::pair<int, int>& c)
+{
+	for (size_t i = 0; i < elListo[c.first].size(); i++)
+	{
+		if (elListo[c.first][i] == c.second)
+		{
+			return false;
+		}
+	}
+	elListo[c.first].push_back(c.second);
+}
+
+bool DirectedGraf::removeConnection(const std::pair<int, int>& c)
+{
+	for (size_t i = 0; i < elListo[c.first].size(); i++)
+	{
+		if (elListo[c.first][i] == c.second)
+		{
+			elListo[c.first].erase(elListo[c.first].begin() + i);
+			return true;
+		}
+	}
+	return false;
+}
+
+int DirectedGraf::getSize() const
+{
+	return elListo.size();
+}
+
+std::vector<int> DirectedGraf::getVertices()
+{
+	std::vector<int> res;
+	for (size_t i = 0; i < elListo.size(); i++)
+	{
+		res.push_back(i);
+	}
+	return res;
+}
+
+std::vector<int> DirectedGraf::getNeighbours(int v)
+{
+	return elListo[v];
+}
+
+int DirectedGraf::getEven()
+{
+	for (size_t i = 0; i < elListo.size(); i++)
+	{
+		if (elListo[i].size() != 0 && elListo[i].size() % 2 == 0)
+			return i;
+	};
+	return -1;
+}
+
--- Algoreturno/Graf.h
+++ Algoreturno/Graf.h
@@ -34,3 +34,37 @@ private:
 
 };
 
+
+
+class DirectedGraf
+{
+public:
+	DirectedGraf();
+
+	DirectedGraf(const std::vector<std::pair<int, int>>& eL, size_t V);
+
+	~DirectedGraf();
+
+	void init(const std::vector<std::pair<int, int>>& eL, size_t V);
+
+	void robercik();		// Krypton
+
+	std::vector<int> flores();			// Polon
+
+	void show();
+
+	bool addConnection(const std::pair<int, int> & c);
+
+	bool removeConnection(const std::pair<int, int> & c);
+
+	int getSize()const;
+
+
+private:
+	std::vector<std::vector<int>> elListo;
+
+	std::vector<int> getVertices();
+	std::vector<int> getNeighbours(int v);
+	int getEven();
+
+};
